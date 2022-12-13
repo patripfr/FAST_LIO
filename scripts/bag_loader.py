5d970e2ae43850bd0493ec49a795deef435174ae
stlucas <stlucas@ethz.ch>
2022-12-13 10:16:54 +0100

    more visualizers

--- scripts/bag_loader.py
+++ scripts/bag_loader.py
@@ -8,106 +8,79 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy
 from scipy.spatial.transform import Rotation as R
+import subprocess
 import open3d as o3d
+import yaml
 
 
 import rosbag
 from rospy import Time
 import ros_numpy
 
+available_msg_types = ["nav_msgs/Odometry",
+                       "geometry_msgs/TransformStamped",
+                       "geometry_msgs/PointStamped",
+                       ]
 
-class TF_Stamped():
-    def __init__(self, transl, quat, stamp = None):
+
+class State():
+    def __init__(self, transl, stamp = None):
         self.stamp = stamp
         self.t = np.asarray([[transl.x, transl.y, transl.z]]).T
-        self.quat = quat
-
+        
+        self.v = None
         self.rot_matrix = None
-
-        self.generateRotMat()
-
-    def generateRotMat(self):
+        self.euler = None
+        
+        self.rot_vel = None
+        
+    def generateOrientations(self):
         self.rot_matrix = R.from_quat([self.quat.x, self.quat.y, self.quat.z, self.quat.w])
+        self.euler = self.rot_matrix.as_euler('zxy', degrees=True)
+        
+    def reset_stamp(self, stamp):
+        self.stamp = self.stamp - stamp
+
+class TF_Stamped(State):
+    def __init__(self, transl, quat, stamp = None):
+        super().__init__(transl, stamp)
+        
+        self.quat = quat
+        self.generateOrientations()
 
     def transformPoint(self, point):
-        #print(self.rot_matrix.as_matrix(), "\n", self.t)
         return np.dot(self.rot_matrix.as_matrix(), point) + self.t
-
-def read_tf_topic(bag, topic):
-    print('Reading topic: '+ topic)
-    data = []
-
-    try:
-        msg_type = bag.get_type_and_topic_info()[1][topic][0]
-    except KeyError:
-        print("Oops!  Topic not found, skipping...")
-        msg_type = "not_found_in_bag"
-
-    for topic, msg, t in bag.read_messages(topics=[topic]):
-        time = np.datetime64(msg.header.stamp.secs, 's')+np.timedelta64(msg.header.stamp.nsecs, 'ns')
-
-        tf = TF_Stamped(msg.transform.translation, msg.transform.rotation, time)
-
-        data.append(tf)
-    return data
-
-
-class Odom():
+        
+class Odom(State):
     def __init__(self, transl, quat, vel, rot_vel, stamp = None):
-        self.stamp = stamp
-        self.t = np.asarray([[transl.x, transl.y, transl.z]]).T
+        super().__init__(transl, stamp)
         self.quat = quat
         self.vel = vel
         self.rot_vel = rot_vel
 
         self.rot_matrix = None
-
-        self.generateRotMat()
-
-    def generateRotMat(self):
-        self.rot_matrix = R.from_quat([self.quat.x, self.quat.y, self.quat.z, self.quat.w])
-
-    def transformPoint(self, point):
-        #print(self.rot_matrix.as_matrix(), "\n", self.t)
-        return np.dot(self.rot_matrix.as_matrix(), point) + self.t
+        self.euler = None
         
-def read_odom_topic(bag, topic):
-    print('Reading topic: '+ topic)
-    data = []
-
-    try:
-        msg_type = bag.get_type_and_topic_info()[1][topic][0]
-    except KeyError:
-        print("Oops!  Topic not found, skipping...")
-        msg_type = "not_found_in_bag"
-
-    for topic, msg, t in bag.read_messages(topics=[topic]):
-        time = np.datetime64(msg.header.stamp.secs, 's')+np.timedelta64(msg.header.stamp.nsecs, 'ns')
-
-        point = Odom(msg.pose.pose.position, msg.pose.pose.orientation, msg.twist.twist.linear, msg.twist.twist.angular, time)
-        data.append(point)
-
-    return data
-
-
-
-class Point():
-    def __init__(self, transl, stamp = None):
-        self.stamp = stamp
-        self.p = np.asarray([[transl.x, transl.y, transl.z]]).T
-        self.t = self.p
-        self.tf = 0
-
-    def generateRotMat(self):
-        self.rot_matrix = R.from_quat([self.quat.x, self.quat.y, self.quat.z, self.quat.w])
+        self.generateOrientations()
 
     def transformPoint(self, point):
         #print(self.rot_matrix.as_matrix(), "\n", self.t)
         return np.dot(self.rot_matrix.as_matrix(), point) + self.t
 
+class Point(State):
+    # default constructor applied
+    def generateOrientations(self):
+        raise Exception("Can't generate rot_mat from point")
+        pass
 
-def read_point_topic(bag, topic):
-    print('Reading topic: '+ topic)
+    def transformPoint(self, point):
+        #print(self.rot_matrix.as_matrix(), "\n", self.t)
+        if(rot_mat is None):
+            raise Exception("Date provided can't be in the past")
+        return np.dot(self.rot_matrix.as_matrix(), point) + self.t
+
+def read_topic(bag, topic):
+    #print('Reading topic: '+ topic)
     data = []
 
     try:
@@ -115,11 +88,33 @@ def read_point_topic(bag, topic):
     except KeyError:
         print("Oops!  Topic not found, skipping...")
         msg_type = "not_found_in_bag"
+        
+    if not msg_type in available_msg_types:
+        print("read " + msg_type + " is not implemented yet")
+        return None
 
     for topic, msg, t in bag.read_messages(topics=[topic]):
-        time = np.datetime64(msg.header.stamp.secs, 's')+np.timedelta64(msg.header.stamp.nsecs, 'ns')
+        time = msg.header.stamp.secs + (10**-9 * msg.header.stamp.nsecs)
+                
+        if msg_type == "nav_msgs/Odometry":
+            element = Odom(msg.pose.pose.position, msg.pose.pose.orientation, msg.twist.twist.linear, msg.twist.twist.angular, time)
+        elif msg_type == "geometry_msgs/TransformStamped":
+            element = TF_Stamped(msg.transform.translation, msg.transform.rotation, time)
+        elif msg_type == "geometry_msgs/PointStamped":
+            element = Point(msg.point, time)
+        else:
+            print("sth wrong with msg_type")
+            break
+            
 
-        point = Point(msg.point, time)
+        data.append(element)
 
-        data.append(point)
+    print("For topic '" + topic + "' available msgs: " + str(len(data)))
     return data
+
+def get_start_time(bag_path):
+      info_dict = yaml.safe_load(subprocess.Popen(['rosbag', 'info', '--yaml', bag_path], stdout=subprocess.PIPE).communicate()[0])
+      duration = info_dict['duration']
+      print("Bag_start", info_dict['start'])
+      start_time = info_dict['start']
+      return start_time
--- scripts/visualize_states.py
+++ scripts/visualize_states.py
@@ -24,63 +24,109 @@ min_range = 1.0
 
 
 def main():
-    vis_odoms()    
-    #vis_corrupted_bag()
+    compare_updates()    
+    compare_corrupted_bag()
     
-def vis_odoms():
+def compare_updates():
     paths = ["/home/lucas/bags/gtsam_fusion/original_prediction_update.bag",
-             "/home/lucas/bags/gtsam_fusion/new_prediction_update.bag",
-             "/home/lucas/bags/gtsam_fusion/new_prediction_update2.bag"]
-    names = ["Original",
-             "Try1",
-             "Try2"]
+             #"/home/lucas/bags/gtsam_fusion/new_prediction_update.bag",
+             #"/home/lucas/bags/gtsam_fusion/new_prediction_update2.bag",
+             "/home/lucas/bags/gtsam_fusion/new_prediction_update3.bag"
+             ]
+   
+    names = [
+             "Original",
+             #"Try1",
+             #"Try2",
+             "Try3"
+             ]
     
     vis_odom(paths, names)
+def compare_corrupted_bag():
+    paths = [
+             #"/home/lucas/bags/gtsam_fusion/original_prediction_update.bag",
+             "/home/lucas/bags/gtsam_fusion/missing_pcl_no_gps.bag"
+             ]
+   
+    names = [
+             #"Original",
+             "Corrupted"
+             ]
+    vis_states(paths, names)
+    
+def vis_states(bag_paths, names):
+    
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(211)# projection='3d')
+    ax1 = fig.add_subplot(212)
+    
+    topics = ['/kolibri/transform_flu',
+              '/Gnss',
+              '/Odometry']
+    
+    topic_names = [": Pose Graph",
+                 ": GNSS",
+                 ": LIO"]
+    
+    stamp_counter = 0
+    for bag_path, name in zip(bag_paths, names):
+        bag = rosbag.Bag(bag_path)
+        
+        for topic, topic_name in zip(topics, topic_names):
+            msg_list = bag_loader.read_topic(bag, topic)
+            plot_state_estimate_2D(msg_list, ax, name + topic_name)
+            plot_time_stamps(msg_list, ax1, stamp_counter, name + topic_name)
+            stamp_counter = stamp_counter + 1
+             
+    ax.legend()
+    ax.axis('equal')
+    ax1.legend()
+    #ax.title("Matching timestamps (but not receipt time!!)")
+    plt.show()
     
 def vis_odom(paths, names):
     # create plot
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111)# projection='3d')
+    fig1 = plt.figure(figsize=(8, 8))
+    fig1_ax1 = plt.subplot2grid(shape=(3, 3), loc=(0, 0), colspan=2, rowspan=2)
+    fig1_ax2 = plt.subplot2grid(shape=(3, 3), loc=(0, 2))
+    fig1_ax3 = plt.subplot2grid(shape=(3, 3), loc=(1, 2))
+    fig1_ax4 = plt.subplot2grid(shape=(3, 3), loc=(2, 2))
+    
+    fig2 = plt.figure(figsize=(8, 8))
+    fig2_ax1 = plt.subplot2grid(shape=(3, 1), loc=(0, 0))
+    fig2_ax2 = plt.subplot2grid(shape=(3, 1), loc=(1, 0))
+    fig2_ax3 = plt.subplot2grid(shape=(3, 1), loc=(2, 0))
     
     for path, name in zip(paths, names):
+        # load bag
         bag = rosbag.Bag(path)
-        points = bag_loader.read_odom_topic(bag, '/Odometry')
-        plot_state_estimate_2D(points, ax, name)
+        odoms = bag_loader.read_topic(bag, '/Odometry')
+        
+        plot_state_estimate_2D(odoms, fig1_ax1, name)
+        plot_orientations(odoms, [fig1_ax2, fig1_ax3, fig1_ax4], name)
+        plot_state_estimate_1D(odoms, [fig2_ax1, fig2_ax2, fig2_ax3], name)
     
-    ax.legend()
+    fig1_ax1.legend()
     plt.show()
         
-def vis_corrupted_bag():
-    bag_path = "/home/lucas/bags/gtsam_fusion/missing_pcl_no_gps.bag"
-    bag = rosbag.Bag(bag_path)
 
-    tfs_lio = bag_loader.read_tf_topic(bag, '/kolibri/transform_flu')
-    print("Found transforms: ", len(tfs_lio))
-    # TODO may reevaluate with /Odometry from LIO directly (transfrom flu comes from mav_state_estimation)
-
-    points_gnss = bag_loader.read_point_topic(bag, '/Gnss')
-    print("Found points: ", len(points_gnss))
     
-    points_odom = bag_loader.read_odom_topic(bag, '/Odometry')
-    print("Found odoms: ", len(points_odom))
+def plot_state_estimate_1D(list_of_containers, ax, label = None):
+    translations = np.empty((3,1))
+    stamps = list()
     
-
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(211)# projection='3d')
+    for container in list_of_containers:
+        translations = np.append(translations, container.t, axis = 1)
+        stamps.append(container.stamp)
+        
+    translations = np.delete(translations, 0, axis = 1)
     
-    plot_state_estimate_2D(tfs_lio, ax, "Pose Graph")
-    plot_state_estimate_2D(points_gnss, ax, "GNSS")
-    plot_state_estimate_2D(points_odom, ax, "LIO")
-    ax.legend()
-    ax.axis('equal')
-    
-    ax1 = fig.add_subplot(212)
-    plot_time_stamps(tfs_lio, ax1, 1, "Pose Graph")
-    plot_time_stamps(points_gnss, ax1, 2, "GNSS")
-    plot_time_stamps(points_odom, ax1, 3, "LIO")
-    ax.legend()
-    #ax.title("Matching timestamps (but not receipt time!!)")
-    plt.show()
+    ax[0].scatter(stamps, translations[0], s= 4, label=label)
+    ax[0].set_title("x_transl")
+    ax[1].scatter(stamps, translations[1], s= 4, label=label)
+    ax[1].set_title("y_transl")
+    ax[2].scatter(stamps, translations[2], s= 4, label=label)
+    ax[2].set_title("z_transl")
     
 def plot_state_estimate_2D(list_of_containers, ax, label = None):
     translations = np.empty((3,1))
@@ -100,16 +146,28 @@ def plot_state_estimate_3D(tfs, ax):
                translations[1],
                translations[2], 
                c='g', s= 4)
+               
+def plot_orientations(container, axes, label = None):
+    rpy = [list(), list(), list()]
+    stamps = list()
+    titles = ["roll", "pitch", "yaw"]
+    
+    for element in container:
+        for angle, sub_list in zip(element.euler, rpy):
+            sub_list.append(angle)
+        
+        stamps.append(element.stamp)
+    
+    for ax, data, title in zip(axes, rpy, titles):
+        ax.scatter(stamps, data, s = 4, label=label)
+        ax.legend()
+        ax.set_title(title)
     
 def plot_time_stamps(list_of_containers, ax, value = 0, label = None):
     times = []
     initial_stamp = list_of_containers[0].stamp
     for container in list_of_containers:
         times.append(container.stamp - initial_stamp)
-    #print(times[::50])
     ax.scatter(times, [value for t in times], label = label)
-        
-    
-    
     
 main()
